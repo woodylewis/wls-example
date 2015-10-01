820a699f1d73d2fc64c86e21884ba9be946f2425
woodylewis <woody@woodylewis.com>
2015-10-01 13:40:46 -0400

    Add function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict'
+/*
+ * wls-example
+*/
 
 module.exports = function(foo) {
 	console.log('INSIDE');
@@ -7,9 +10,15 @@ module.exports = function(foo) {
 
 	foo.one = 'gamma';
 	foo.two = 'delta';
-	
+
+	var wls_func = function(req, res, next) {
+		console.log('WLS_FUNC - ', req);
+		res.json({display: 'WLS_FUNC'});
+	};
+
 	return {
 		one:foo.one,
-		two:foo.two
+		two:foo.two,
+		wls_func: wls_func
 	};
 };
